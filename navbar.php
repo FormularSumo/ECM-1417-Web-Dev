4ba369724a6b54481610cb52efe259c7b81c1c32
FormularSumo <chewy.h@live.co.uk>
2023-05-01 11:49:04 +0100

    Remove unused getCookie function
    Leftover from when I was testing navbar being a js file

--- navbar.php
+++ navbar.php
@@ -16,21 +16,6 @@
 </ul>
 
 <script>
-  function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i <ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
-  }
   window.onload = function() {
     document.getElementById("skin").src = 'emoji assets/skin/' + getCookie("skin") + '.png';
     document.getElementById("eyes").src = 'emoji assets/eyes/' + getCookie("eyes") + '.png';
