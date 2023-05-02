bc37260415205e1766e9446aa0a069c911cc693f
FormularSumo <chewy.h@live.co.uk>
2023-05-02 01:49:23 +0100

    Prevent dragging cards and card being flipped back over by double pressing it
    Both to prevent cheating, as dragging could let you see a flipped card longer and double pressing could let you check every card (and not how original game works)

--- pairs.php
+++ pairs.php
@@ -20,6 +20,9 @@
         window.requestAnimationFrame(update);
         for (const child of document.getElementById('level 1').children) {
           child.addEventListener("click",() => clicked(child));
+          for (const child2 of child.children) { //prevent user from dragging picture as they could then see card for longer
+            child2.draggable=false;
+          }
         }
       }
 
@@ -57,25 +60,24 @@
           }
           if (card.timer) {
             card.timer.clear();
-          }
-          if (card_flipped == card) {
-            for (const child of card_flipped.children) {child.style.opacity=0;} //If same card is clicked again to cancel, flip instantly as player has already seen it
-          } else if (card_flipped != card && card.children[0].src === card_flipped.children[0].src && card.children[1].src === card_flipped.children[1].src && card.children[2].src === card_flipped.children[2].src) {
-            game_active = false;
-            for (const child of card_flipped.children) {
-              child.style.opacity=1;
+          } else if (card_flipped != card) {
+            if (card_flipped != card && card.children[0].src === card_flipped.children[0].src && card.children[1].src === card_flipped.children[1].src && card.children[2].src === card_flipped.children[2].src) {
+              game_active = false;
+              for (const child of card_flipped.children) {
+                child.style.opacity=1;
+              }
+              for (const child of card.children) {
+                child.style.opacity=1;
+              }
+            } else {
+              const card_to_be_cleared = card; //Consts are used so that card_flipped can be cleared and this function can be run again
+              card.timer = createTimeout(function() {card_to_be_cleared.timer = null; for (const child of card_to_be_cleared.children) {child.style.opacity=0;}}, 400); //400ms delay so that user can see the card they've just flipped
+              const card_to_be_cleared2 = card_flipped;
+              card_flipped.timer = createTimeout(function() {card_to_be_cleared2.timer = null;for (const child of card_to_be_cleared2.children) {child.style.opacity=0;}}, 400);
+              current_points = current_points * 0.95;
             }
-            for (const child of card.children) {
-              child.style.opacity=1;
-            }
-          } else {
-            const card_to_be_cleared = card; //Consts are used so that card_flipped can be cleared and this function can be run again
-            card.timer = createTimeout(function() {card_to_be_cleared.timer = null; for (const child of card_to_be_cleared.children) {child.style.opacity=0;}}, 400); //400ms delay so that user can see the card they've just flipped
-            const card_to_be_cleared2 = card_flipped;
-            card_flipped.timer = createTimeout(function() {card_to_be_cleared2.timer = null;for (const child of card_to_be_cleared2.children) {child.style.opacity=0;}}, 400);
-            current_points = current_points * 0.95;
+            card_flipped = false;
           }
-          card_flipped = false;
         }
       }
 
@@ -106,9 +108,9 @@
           <p id='point counter'>Points: 1000</p>
           <div id='level 1' class='grid' style='grid-template-columns: repeat(3, 1fr)'>
             <div class='card' style='grid-column: 1; grid-row: 1;'>
-              <img src="emoji assets/skin/yellow.png" height=80px style="grid-column: 1; grid-row: 1; z-index: 0;">
-              <img src="emoji assets/eyes/closed.png" height=80px style="grid-column: 1; grid-row: 1; z-index: 1;">
-              <img src="emoji assets/mouth/open.png" height=80px style="grid-column: 1; grid-row: 1; z-index: 2;">
+              <img src="emoji assets/skin/yellow.png" height=80px style="grid-column: 1; grid-row: 1; z-index: 0;" draggable="false">
+              <img src="emoji assets/eyes/closed.png" height=80px style="grid-column: 1; grid-row: 1; z-index: 1;" draggable="false">
+              <img src="emoji assets/mouth/open.png" height=80px style="grid-column: 1; grid-row: 1; z-index: 2;" draggable="false">
             </div>
             <div class='card' style='grid-column: 2; grid-row: 1;'>
               <img src="emoji assets/skin/green.png" height=80px style="grid-column: 1; grid-row: 1; z-index: 0;">
